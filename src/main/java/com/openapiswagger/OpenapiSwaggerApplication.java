012404390eee72d24eabb0012f6ba2bc510f195a
sdgamer007 <sohaildua38@gmail.com>
2023-12-08 01:29:00 +0100

    Create a empty openapi project

---
+++ src/main/java/com/openapiswagger/OpenapiSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.openapiswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenapiSwaggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openapiswagger/OpenapiSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openapiswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
